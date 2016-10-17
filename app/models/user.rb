f8fb4dc1e69ce67cb694cb35dc8590dd0d7505f4
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-16 18:01:17 -0700

    Added test for numericality of age, added validation to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
+  validates :age, numericality: {only_integer: true}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "should not save user without name" do 
-    user = User.new
+    user = User.new(email: "Fish", age: 4)
     assert_not user.save
   end
 
   test "should not save user without email" do 
-    user = User.new
+    user = User.new(name: "Fred", age: 4)
+    assert_not user.save
+  end
+
+  test "should not save user without integer age" do 
+    user = User.new(name: 4, email: "Fish", age: "Sandwich")
     assert_not user.save
   end
 
